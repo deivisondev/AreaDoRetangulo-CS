8d37cf02964dbed4a6f62974a2b13f00fa1b6a60
deivisondev <deivisoneditordevideos@gmail.com>
2022-07-14 11:32:26 -0300

    Início

---
+++ AreaDoRetangulo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace RectangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle();
+
+            Console.WriteLine("Entre a largura e altura do retângulo:");
+            rectangle.RectangleWidth = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            rectangle.RectangleHeight = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine($"\nÁREA = {rectangle.Area().ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"\nPERÍMETRO = {rectangle.Perimeter().ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"\nDIAGONAL = {rectangle.Diagonal().ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
---
+++ AreaDoRetangulo/Rectangle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace RectangleArea
+{
+    public class Rectangle
+    {
+        public double RectangleWidth;
+        public double RectangleHeight;
+
+        public double Area()
+        {
+            return RectangleWidth * RectangleHeight;
+        }
+
+        public double Perimeter()
+        {
+            return 2 * (RectangleWidth = RectangleHeight);
+        }
+
+        public double Diagonal()
+        {
+            return Math.Sqrt(Math.Pow(RectangleWidth, 2) + Math.Pow(RectangleHeight, 2));
+        }
+    }
+}
